631a8aea6fb24fe88ed7e599ebdf3c8b7f9d54da
Tobias Olsson <tobias.olsson@pm.me>
2021-04-10 20:47:26 +0200

    Edit some rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -25,5 +25,6 @@ module.exports = {
     'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
     'react/forbid-prop-types': [0, { forbid: ['any'] }],
     'react/prop-types': 0,
+    'react/destructuring-assignment': 1,
   },
 };
