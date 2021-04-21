b67f6b30d0c0590695a95a19f6f52d66b845943d
Tobias Olsson <tobias.olsson@pm.me>
2021-04-21 15:42:12 +0200

    Simplify sunrise/set into one component and style

--- src/components/forecast/Forecast.js
+++ src/components/forecast/Forecast.js
@@ -2,8 +2,7 @@ import React from 'react';
 import Temp from './Temp';
 import FeelsLike from './FeelsLike';
 import Humidity from './Humidity';
-import Sunrise from './Sunrise';
-import Sunset from './Sunset';
+import Sun from './Sun';
 import City from './City';
 import Clouds from './Clouds';
 import styles from './Forecast.module.css';
@@ -17,8 +16,7 @@ const Forecast = (props) => {
       <Clouds clouds={clouds} />
       <FeelsLike feels={feelsLike} />
       <Humidity humidity={humidity} />
-      <Sunrise sunrise={sunrise} />
-      <Sunset sunset={sunset} />
+      <Sun sunset={sunset} sunrise={sunrise} />
     </div>
   );
 };
---
+++ src/components/forecast/Sun.js
@@ -0,0 +1,23 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import { convertUnixTime } from '../../utility';
+import styles from './Sun.module.css';
+
+const Sun = (props) => {
+  const sunset = convertUnixTime(props.sunset);
+  const sunrise = convertUnixTime(props.sunrise);
+
+  return (
+    <ul className={styles.Sun}>
+      <li className={styles.Risebox}>
+        <span className={`${styles.SunIcon} ${styles.Sunrise}`}>wb_sunny</span>{' '}
+        {sunrise}
+      </li>
+      <li className={styles.Setbox}>
+        <span className={`${styles.SunIcon} ${styles.Sunset}`}>wb_sunny</span>{' '}
+        {sunset}
+      </li>
+    </ul>
+  );
+};
+export default Sun;
